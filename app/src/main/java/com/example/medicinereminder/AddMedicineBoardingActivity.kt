9d76b0db97e92a3a4a996b83be5d6a26ed468ffc
aisjac <[shaheenmirja143@gmail.com]>
2021-10-29 17:59:38 +0600

    adding bottom navigation done

--- app/src/main/java/com/example/medicinereminder/AddMedicineBoardingActivity.kt
+++ app/src/main/java/com/example/medicinereminder/AddMedicineBoardingActivity.kt
@@ -1,11 +1,18 @@
 package com.example.medicinereminder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.medicinereminder.databinding.ActivityAddMedicineBoardingBinding
 
 class AddMedicineBoardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val addMedicineBinding :ActivityAddMedicineBoardingBinding = ActivityAddMedicineBoardingBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_medicine_boarding)
+        setContentView(addMedicineBinding.root)
+
+        addMedicineBinding.addMedicineButtonId.setOnClickListener {
+            startActivity(Intent(this,DashboardActivity::class.java))
+        }
     }
 }
