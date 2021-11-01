2dfdd4ea93b08e56af83371f1f03787986445d07
aisjac <[shaheenmirja143@gmail.com]>
2021-11-01 18:54:38 +0600

    addMedicine first three page design done.Dialog and chips design running

--- app/src/main/java/com/example/medicinereminder/AddMedicineStepTwoFragment.kt
+++ app/src/main/java/com/example/medicinereminder/AddMedicineStepTwoFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.navigation.fragment.findNavController
 import com.example.medicinereminder.databinding.FragmentAddMedicineStepOneBinding
 import com.example.medicinereminder.databinding.FragmentAddMedicineStepTwoBinding
@@ -19,26 +21,36 @@ class AddMedicineStepTwoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
         _binding = FragmentAddMedicineStepTwoBinding.inflate(inflater, container, false)
 
 
-        binding.textViewStepTwoFragmentTVId.setOnClickListener {
-            findNavController().navigate(R.id.action_addMedicineStepTwoFragment_to_addMedicineStepThreeFragment)
+        val options = arrayOf("0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31")
+        val optionsDMY = arrayOf("দিন","মাস","বছর")
+
+
+        binding.addMedicineNumberSpinnerId.adapter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_item,options)
+        binding.addMedicineDMYSpinnerId.adapter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_item,optionsDMY)
+
+
+
+
+            binding.textViewStepTwoFragmentTVId.setOnClickListener {
+                findNavController().navigate(R.id.action_addMedicineStepTwoFragment_to_addMedicineStepThreeFragment)
+            }
+
+            return binding.root
         }
 
 
 
+    fun showDialog() {
 
 
-
-
-        return binding.root
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+
 }
