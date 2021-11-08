d8d2195bf980aaddb26b9de58aabbbbeebc5989f
aisjac <shaheenmirja143@gmail.com>
2021-11-08 12:57:50 +0600

    drawer fragment connectivity  and drawer header issue solved

--- app/src/main/java/com/example/medicinereminder/DashboardActivity.kt
+++ app/src/main/java/com/example/medicinereminder/DashboardActivity.kt
@@ -2,10 +2,13 @@ package com.example.medicinereminder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.medicinereminder.databinding.ActivityDashboardBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +17,12 @@ class DashboardActivity : AppCompatActivity() {
 
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationViewId)
+        val navigationDrawer = findViewById<NavigationView>(R.id.navigationView_drawerId)
         val navController = findNavController(R.id.fragmentContainerView)
 
         bottomNavigationView.setupWithNavController(navController)
+
+        NavigationUI.setupWithNavController(navigationDrawer,navController)
 
 
     }
